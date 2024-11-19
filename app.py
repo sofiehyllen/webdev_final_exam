9448cecbb4b0e13bd81fce0928d463d9d26b4b96
sofiehyllen <sofiehyllen@hotmail.com>
2024-11-19 15:05:31 +0100

    initial

---
+++ app.py
@@ -0,0 +1,496 @@
+from flask import Flask, session, render_template, redirect, url_for, make_response, request
+from flask_session import Session
+from werkzeug.security import generate_password_hash
+from werkzeug.security import check_password_hash
+import x
+import uuid 
+import time
+import redis
+import os
+
+from icecream import ic
+ic.configureOutput(prefix=f'***** | ', includeContext=True)
+
+app = Flask(__name__)
+app.config['SESSION_TYPE'] = 'filesystem'  # or 'redis', etc.
+Session(app)
+
+
+# app.secret_key = "your_secret_key"
+
+##############################
+##############################
+##############################
+
+def _________GET_________(): pass
+
+##############################
+##############################
+
+##############################
+@app.get("/test-set-redis")
+def view_test_set_redis():
+    redis_host = "redis"
+    redis_port = 6379
+    redis_client = redis.Redis(host=redis_host, port=redis_port, decode_responses=True)    
+    redis_client.set("name", "Santiago", ex=10)
+    # name = redis_client.get("name")
+    return "name saved"
+
+@app.get("/test-get-redis")
+def view_test_get_redis():
+    redis_host = "redis"
+    redis_port = 6379
+    redis_client = redis.Redis(host=redis_host, port=redis_port, decode_responses=True)    
+    name = redis_client.get("name")
+    if not name: name = "no name"
+    return name
+
+##############################
+@app.get("/")
+def view_index():
+    name = "X"
+    return render_template("view_index.html", name=name)
+
+##############################
+@app.get("/signup")
+@x.no_cache
+def view_signup():  
+    ic(session)
+    if session.get("user"):
+        if len(session.get("user").get("roles")) > 1:
+            return redirect(url_for("view_choose_role")) 
+        if "admin" in session.get("user").get("roles"):
+            return redirect(url_for("view_admin"))
+        if "customer" in session.get("user").get("roles"):
+            return redirect(url_for("view_customer")) 
+        if "partner" in session.get("user").get("roles"):
+            return redirect(url_for("view_partner"))         
+    return render_template("view_signup.html", x=x, title="Signup")
+
+
+##############################
+@app.get("/login")
+@x.no_cache
+def view_login():  
+    # ic("#"*20, "VIEW_LOGIN")
+    ic(session)
+    # print(session, flush=True)  
+    if session.get("user"):
+        if len(session.get("user").get("roles")) > 1:
+            return redirect(url_for("view_choose_role")) 
+        if "admin" in session.get("user").get("roles"):
+            return redirect(url_for("view_admin"))
+        if "customer" in session.get("user").get("roles"):
+            return redirect(url_for("view_customer")) 
+        if "partner" in session.get("user").get("roles"):
+            return redirect(url_for("view_partner"))         
+    return render_template("view_login.html", x=x, title="Login", message=request.args.get("message", ""))
+
+
+##############################
+@app.get("/customer")
+@x.no_cache
+def view_customer():
+    if not session.get("user", ""): 
+        return redirect(url_for("view_login"))
+    user = session.get("user")
+    if len(user.get("roles", "")) > 1:
+        return redirect(url_for("view_choose_role"))
+    return render_template("view_customer.html", user=user)
+
+##############################
+@app.get("/partner")
+@x.no_cache
+def view_partner():
+    if not session.get("user", ""): 
+        return redirect(url_for("view_login"))
+    user = session.get("user")
+    if len(user.get("roles", "")) > 1:
+        return redirect(url_for("view_choose_role"))
+    return response
+
+
+##############################
+@app.get("/admin")
+@x.no_cache
+def view_admin():
+    if not session.get("user", ""): 
+        return redirect(url_for("view_login"))
+    user = session.get("user")
+    if not "admin" in user.get("roles", ""):
+        return redirect(url_for("view_login"))
+    return render_template("view_admin.html")
+
+
+
+##############################
+@app.get("/choose-role")
+@x.no_cache
+def view_choose_role():
+    if not session.get("user", ""): 
+        return redirect(url_for("view_login"))
+    if not len(session.get("user").get("roles")) >= 2:
+        return redirect(url_for("view_login"))
+    user = session.get("user")
+    return render_template("view_choose_role.html", user=user, title="Choose role")
+
+
+##############################
+##############################
+##############################
+
+def _________POST_________(): pass
+
+##############################
+##############################
+##############################
+
+@app.post("/logout")
+def logout():
+    # ic("#"*30)
+    # ic(session)
+    session.pop("user", None)
+    # session.clear()
+    # session.modified = True
+    # ic("*"*30)
+    # ic(session)
+    return redirect(url_for("view_login"))
+
+
+##############################
+@app.post("/users")
+@x.no_cache
+def signup():
+    try:
+        user_name = x.validate_user_name()
+        user_last_name = x.validate_user_last_name()
+        user_email = x.validate_user_email()
+        user_password = x.validate_user_password()
+        hashed_password = generate_password_hash(user_password)
+        
+        user_pk = str(uuid.uuid4())
+        user_created_at = int(time.time())
+        user_deleted_at = 0
+        user_blocked_at = 0
+        user_updated_at = 0
+        user_verified_at = 0
+        user_verification_key = str(uuid.uuid4())
+
+        db, cursor = x.db()
+
+        default_role = "customer"
+        cursor.execute('SELECT role_pk FROM roles WHERE role_name = %s', (default_role,))
+        result = cursor.fetchone()
+
+        user_role = result["role_pk"]
+
+        q = 'INSERT INTO users VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'
+        cursor.execute(q, (user_pk, user_name, user_last_name, user_email, 
+                            hashed_password, user_created_at, user_deleted_at, user_blocked_at, 
+                            user_updated_at, user_verified_at, user_verification_key))
+        
+        q_roles = 'INSERT INTO users_roles (user_role_user_fk, user_role_role_fk) VALUES (%s, %s)'
+        cursor.execute(q_roles, (user_pk, user_role))
+
+        # x.send_verify_email(user_email, user_verification_key)
+
+        db.commit()
+    
+        return """<template mix-redirect="/login"></template>""", 201
+    
+    except Exception as ex:
+        ic(ex)
+        if "db" in locals(): db.rollback()
+        if isinstance(ex, x.CustomException): 
+            toast = render_template("___toast.html", message=ex.message)
+            return f"""<template mix-target="#toast" mix-bottom>{toast}</template>""", ex.code    
+        if isinstance(ex, x.mysql.connector.Error):
+            ic(ex)
+            if "users.user_email" in str(ex): 
+                toast = render_template("___toast.html", message="email not available")
+                return f"""<template mix-target="#toast" mix-bottom>{toast}</template>""", 400
+            return f"""<template mix-target="#toast" mix-bottom>System upgrating</template>""", 500        
+        return f"""<template mix-target="#toast" mix-bottom>System under maintenance</template>""", 500    
+    finally:
+        if "cursor" in locals(): cursor.close()
+        if "db" in locals(): db.close()
+
+
+##############################
+@app.post("/login")
+def login():
+    try:
+
+        user_email = x.validate_user_email()
+        user_password = x.validate_user_password()
+
+        db, cursor = x.db()
+        q = """ SELECT * FROM users 
+                JOIN users_roles 
+                ON user_pk = user_role_user_fk 
+                JOIN roles
+                ON role_pk = user_role_role_fk
+                WHERE user_email = %s"""
+        cursor.execute(q, (user_email,))
+        rows = cursor.fetchall()
+        if not rows:
+            toast = render_template("___toast.html", message="user not registered")
+            return f"""<template mix-target="#toast">{toast}</template>""", 400     
+        if not check_password_hash(rows[0]["user_password"], user_password):
+            toast = render_template("___toast.html", message="invalid credentials")
+            return f"""<template mix-target="#toast">{toast}</template>""", 401
+        
+        roles = []
+        for row in rows:
+            roles.append(row["role_name"])
+        user = {
+            "user_pk": rows[0]["user_pk"],
+            "user_name": rows[0]["user_name"],
+            "user_last_name": rows[0]["user_last_name"],
+            "user_email": rows[0]["user_email"],
+            "roles": roles
+        }
+
+        ic(user)
+        session["user"] = user
+        if len(roles) == 1:
+            return f"""<template mix-redirect="/{roles[0]}"></template>"""
+        return f"""<template mix-redirect="/choose-role"></template>"""
+    except Exception as ex:
+        ic(ex)
+        if "db" in locals(): db.rollback()
+        if isinstance(ex, x.CustomException): 
+            toast = render_template("___toast.html", message=ex.message)
+            return f"""<template mix-target="#toast" mix-bottom>{toast}</template>""", ex.code    
+        if isinstance(ex, x.mysql.connector.Error):
+            ic(ex)
+            return "<template>System upgrating</template>", 500        
+        return "<template>System under maintenance</template>", 500  
+    finally:
+        if "cursor" in locals(): cursor.close()
+        if "db" in locals(): db.close()
+
+
+##############################
+@app.post("/items")
+def create_item():
+    try:
+        # TODO: validate item_title, item_description, item_price
+        file, item_image_name = x.validate_item_image()
+
+        # Save the image
+        file.save(os.path.join(x.UPLOAD_ITEM_FOLDER, item_image_name))
+        # TODO: if saving the image went wrong, then rollback by going to the exception
+        # TODO: Success, commit
+        return item_image_name
+    except Exception as ex:
+        ic(ex)
+        if "db" in locals(): db.rollback()
+        if isinstance(ex, x.CustomException): 
+            toast = render_template("___toast.html", message=ex.message)
+            return f"""<template mix-target="#toast" mix-bottom>{toast}</template>""", ex.code    
+        if isinstance(ex, x.mysql.connector.Error):
+            ic(ex)
+            return "<template>System upgrating</template>", 500        
+        return "<template>System under maintenance</template>", 500  
+    finally:
+        if "cursor" in locals(): cursor.close()
+        if "db" in locals(): db.close()    
+
+
+##############################
+##############################
+##############################
+
+def _________PUT_________(): pass
+
+##############################
+##############################
+##############################
+
+@app.put("/users")
+def user_update():
+    try:
+        if not session.get("user"): x.raise_custom_exception("please login", 401)
+
+        user_pk = session.get("user").get("user_pk")
+        user_name = x.validate_user_name()
+        user_last_name = x.validate_user_last_name()
+        user_email = x.validate_user_email()
+
+        user_updated_at = int(time.time())
+
+        db, cursor = x.db()
+        q = """ UPDATE users
+                SET user_name = %s, user_last_name = %s, user_email = %s, user_updated_at = %s
+                WHERE user_pk = %s
+            """
+        cursor.execute(q, (user_name, user_last_name, user_email, user_updated_at, user_pk))
+        if cursor.rowcount != 1: x.raise_custom_exception("cannot update user", 401)
+        db.commit()
+        return """<template>user updated</template>"""
+    except Exception as ex:
+        ic(ex)
+        if "db" in locals(): db.rollback()
+        if isinstance(ex, x.CustomException): return f"""<template mix-target="#toast" mix-bottom>{ex.message}</template>""", ex.code
+        if isinstance(ex, x.mysql.connector.Error):
+            if "users.user_email" in str(ex): return "<template>email not available</template>", 400
+            return "<template>System upgrating</template>", 500        
+        return "<template>System under maintenance</template>", 500    
+    finally:
+        if "cursor" in locals(): cursor.close()
+        if "db" in locals(): db.close()
+
+
+##############################
+@app.put("/users/block/<user_pk>")
+def user_block(user_pk):
+    try:        
+        if not "admin" in session.get("user").get("roles"): return redirect(url_for("view_login"))
+        user_pk = x.validate_uuid4(user_pk)
+        user_blocked_at = int(time.time())
+        db, cursor = x.db()
+        q = 'UPDATE users SET user_blocked_at = %s WHERE user_pk = %s'
+        cursor.execute(q, (user_blocked_at, user_pk))
+        if cursor.rowcount != 1: x.raise_custom_exception("cannot block user", 400)
+        db.commit()
+        return """<template>user blocked</template>"""
+    
+    except Exception as ex:
+        ic(ex)
+        if "db" in locals(): db.rollback()
+        if isinstance(ex, x.CustomException): 
+            return f"""<template mix-target="#toast" mix-bottom>{ex.message}</template>""", ex.code        
+        if isinstance(ex, x.mysql.connector.Error):
+            ic(ex)
+            return "<template>Database error</template>", 500        
+        return "<template>System under maintenance</template>", 500  
+    finally:
+        if "cursor" in locals(): cursor.close()
+        if "db" in locals(): db.close()
+
+
+##############################
+@app.put("/users/unblock/<user_pk>")
+def user_unblock(user_pk):
+    try:
+        if not "admin" in session.get("user").get("roles"): return redirect(url_for("view_login"))
+        user_pk = x.validate_uuid4(user_pk)
+        user_blocked_at = 0
+        db, cursor = x.db()
+        q = 'UPDATE users SET user_blocked_at = %s WHERE user_pk = %s'
+        cursor.execute(q, (user_blocked_at, user_pk))
+        if cursor.rowcount != 1: x.raise_custom_exception("cannot unblock user", 400)
+        db.commit()
+        return """<template>user unblocked</template>"""
+    
+    except Exception as ex:
+
+        ic(ex)
+        if "db" in locals(): db.rollback()
+        if isinstance(ex, x.CustomException): 
+            return f"""<template mix-target="#toast" mix-bottom>{ex.message}</template>""", ex.code        
+        if isinstance(ex, x.mysql.connector.Error):
+            ic(ex)
+            return "<template>Database error</template>", 500        
+        return "<template>System under maintenance</template>", 500  
+    
+    finally:
+        if "cursor" in locals(): cursor.close()
+        if "db" in locals(): db.close()
+
+
+
+
+##############################
+##############################
+##############################
+
+def _________DELETE_________(): pass
+
+##############################
+##############################
+##############################
+
+
+@app.delete("/users/<user_pk>")
+def user_delete(user_pk):
+    try:
+        # Check if user is logged
+        if not session.get("user", ""): return redirect(url_for("view_login"))
+        # Check if it is an admin
+        if not "admin" in session.get("user").get("roles"): return redirect(url_for("view_login"))
+        user_pk = x.validate_uuid4(user_pk)
+        user_deleted_at = int(time.time())
+        db, cursor = x.db()
+        q = 'UPDATE users SET user_deleted_at = %s WHERE user_pk = %s'
+        cursor.execute(q, (user_deleted_at, user_pk))
+        if cursor.rowcount != 1: x.raise_custom_exception("cannot delete user", 400)
+        db.commit()
+        return """<template>user deleted</template>"""
+    
+    except Exception as ex:
+
+        ic(ex)
+        if "db" in locals(): db.rollback()
+        if isinstance(ex, x.CustomException): 
+            return f"""<template mix-target="#toast" mix-bottom>{ex.message}</template>""", ex.code        
+        if isinstance(ex, x.mysql.connector.Error):
+            ic(ex)
+            return "<template>Database error</template>", 500        
+        return "<template>System under maintenance</template>", 500  
+    
+    finally:
+        if "cursor" in locals(): cursor.close()
+        if "db" in locals(): db.close()
+
+
+
+
+##############################
+##############################
+##############################
+
+def _________BRIDGE_________(): pass
+
+##############################
+##############################
+##############################
+
+
+##############################
+@app.get("/verify/<verification_key>")
+@x.no_cache
+def verify_user(verification_key):
+    try:
+        ic(verification_key)
+        verification_key = x.validate_uuid4(verification_key)
+        user_verified_at = int(time.time())
+
+        db, cursor = x.db()
+        q = """ UPDATE users 
+                SET user_verified_at = %s 
+                WHERE user_verification_key = %s"""
+        cursor.execute(q, (user_verified_at, verification_key))
+        if cursor.rowcount != 1: x.raise_custom_exception("cannot verify account", 400)
+        db.commit()
+        return redirect(url_for("view_login", message="User verified, please login"))
+
+    except Exception as ex:
+        ic(ex)
+        if "db" in locals(): db.rollback()
+        if isinstance(ex, x.CustomException): return ex.message, ex.code    
+        if isinstance(ex, x.mysql.connector.Error):
+            ic(ex)
+            return "Database under maintenance", 500        
+        return "System under maintenance", 500  
+    finally:
+        if "cursor" in locals(): cursor.close()
+        if "db" in locals(): db.close()    
+
+
+
+
+
+
---
+++ x.py
@@ -0,0 +1,193 @@
+from flask import request, make_response
+from functools import wraps
+import mysql.connector
+import re
+import os
+import uuid
+
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+from icecream import ic
+ic.configureOutput(prefix=f'***** | ', includeContext=True)
+
+UNSPLASH_ACCESS_KEY = 'YOUR_KEY_HERE'
+ADMIN_ROLE_PK = "16fd2706-8baf-433b-82eb-8c7fada847da"
+CUSTOMER_ROLE_PK = "c56a4180-65aa-42ec-a945-5fd21dec0538"
+PARTNER_ROLE_PK = "f47ac10b-58cc-4372-a567-0e02b2c3d479"
+RESTAURANT_ROLE_PK = "9f8c8d22-5a67-4b6c-89d7-58f8b8cb4e15"
+
+
+# form to get data from input fields
+# args to get data from the url
+# values to get data from the url and from the form
+
+class CustomException(Exception):
+    def __init__(self, message, code):
+        super().__init__(message)  # Initialize the base class with the message
+        self.message = message  # Store additional information (e.g., error code)
+        self.code = code  # Store additional information (e.g., error code)
+
+def raise_custom_exception(error, status_code):
+    raise CustomException(error, status_code)
+
+
+##############################
+def db():
+    db = mysql.connector.connect(
+        host="mysql",      # Replace with your MySQL server's address or docker service name "mysql"
+        user="root",  # Replace with your MySQL username
+        password="password",  # Replace with your MySQL password
+        database="company"   # Replace with your MySQL database name
+    )
+    cursor = db.cursor(dictionary=True)
+    return db, cursor
+
+
+##############################
+def no_cache(view):
+    @wraps(view)
+    def no_cache_view(*args, **kwargs):
+        response = make_response(view(*args, **kwargs))
+        response.headers["Cache-Control"] = "no-store, no-cache, must-revalidate, max-age=0"
+        response.headers["Pragma"] = "no-cache"
+        response.headers["Expires"] = "0"
+        return response
+    return no_cache_view
+
+
+##############################
+
+def allow_origin(origin="*"):
+    def decorator(f):
+        @wraps(f)
+        def decorated_function(*args, **kwargs):
+            # Call the wrapped function
+            response = make_response(f(*args, **kwargs))
+            # Add Access-Control-Allow-Origin header to the response
+            response.headers["Access-Control-Allow-Origin"] = origin
+            # Optionally allow other methods and headers for full CORS support
+            response.headers["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS, PUT, DELETE"
+            response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
+            return response
+        return decorated_function
+    return decorator
+
+
+##############################
+USER_NAME_MIN = 2
+USER_NAME_MAX = 20
+USER_NAME_REGEX = f"^.{{{USER_NAME_MIN},{USER_NAME_MAX}}}$"
+def validate_user_name():
+    error = f"name {USER_NAME_MIN} to {USER_NAME_MAX} characters"
+    user_name = request.form.get("user_name", "").strip()
+    if not re.match(USER_NAME_REGEX, user_name): raise_custom_exception(error, 400)
+    return user_name
+
+##############################
+USER_LAST_NAME_MIN = 2
+USER_LAST_NAME_MAX = 20
+USER_LAST_NAME_REGEX = f"^.{{{USER_LAST_NAME_MIN},{USER_LAST_NAME_MAX}}}$"
+def validate_user_last_name():
+    error = f"last name {USER_LAST_NAME_MIN} to {USER_LAST_NAME_MAX} characters"
+    user_last_name = request.form.get("user_last_name", "").strip() # None
+    if not re.match(USER_LAST_NAME_REGEX, user_last_name): raise_custom_exception(error, 400)
+    return user_last_name
+
+##############################
+REGEX_EMAIL = "^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$"
+def validate_user_email():
+    error = "email invalid"
+    user_email = request.form.get("user_email", "").strip()
+    if not re.match(REGEX_EMAIL, user_email): raise_custom_exception(error, 400)
+    return user_email
+
+##############################
+USER_PASSWORD_MIN = 8
+USER_PASSWORD_MAX = 50
+REGEX_USER_PASSWORD = f"^.{{{USER_PASSWORD_MIN},{USER_PASSWORD_MAX}}}$"
+def validate_user_password():
+    error = f"password {USER_PASSWORD_MIN} to {USER_PASSWORD_MAX} characters"
+    user_password = request.form.get("user_password", "").strip()
+    if not re.match(REGEX_USER_PASSWORD, user_password): raise_custom_exception(error, 400)
+    return user_password
+
+##############################
+REGEX_UUID4 = "^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$"
+def validate_uuid4(uuid4 = ""):
+    error = f"invalid uuid4"
+    if not uuid4:
+        uuid4 = request.values.get("uuid4", "").strip()
+    if not re.match(REGEX_UUID4, uuid4): raise_custom_exception(error, 400)
+    return uuid4
+
+##############################
+USER_ROLES = ["admin", "customer", "partner", "restaurant"]
+def validate_user_role():
+    error = "role is missing"
+    user_role = request.form.get("user_role", "")
+    if user_role not in USER_ROLES: raise_custom_exception(error, 400)
+    return user_role
+
+
+
+##############################
+UPLOAD_ITEM_FOLDER = './images'
+ALLOWED_ITEM_FILE_EXTENSIONS = {"png", "jpg", "jpeg", "gif"}
+
+def validate_item_image():
+    if 'item_file' not in request.files: raise_custom_exception("item_file missing", 400)
+    file = request.files.get("item_file", "")
+    if file.filename == "": raise_custom_exception("item_file name invalid", 400)
+
+    if file:
+        ic(file.filename)
+        file_extension = os.path.splitext(file.filename)[1][1:]
+        ic(file_extension)
+        if file_extension not in ALLOWED_ITEM_FILE_EXTENSIONS: raise_custom_exception("item_file invalid extension", 400)
+        filename = str(uuid.uuid4()) + file_extension
+        return file, filename 
+
+##############################
+def send_verify_email(to_email, user_verification_key):
+    try:
+        # Create a gmail fullflaskdemomail
+        # Enable (turn on) 2 step verification/factor in the google account manager
+        # Visit: https://myaccount.google.com/apppasswords
+
+
+        # Email and password of the sender's Gmail account
+        sender_email = "sofiefuglsanghyllen@gmail.com"
+        password = "jsdfsqosuxtlakab"  # If 2FA is on, use an App Password instead
+
+        # Receiver email address
+        receiver_email = to_email
+        
+        # Create the email message
+        message = MIMEMultipart()
+        message["From"] = "My company name"
+        message["To"] = receiver_email
+        message["Subject"] = "Please verify your account"
+
+        # Body of the email
+        body = f"""To verify your account, please <a href="http://127.0.0.1/verify/{user_verification_key}">click here</a>"""
+        message.attach(MIMEText(body, "html"))
+
+        # Connect to Gmail's SMTP server and send the email
+        with smtplib.SMTP("smtp.gmail.com", 587) as server:
+            server.starttls()  # Upgrade the connection to secure
+            server.login(sender_email, password)
+            server.sendmail(sender_email, receiver_email, message.as_string())
+        print("Email sent successfully!")
+
+        return "email sent"
+
+    except Exception as ex:
+        raise_custom_exception("cannot send email", 500)
+    finally:
+        pass
+
+
+
+
