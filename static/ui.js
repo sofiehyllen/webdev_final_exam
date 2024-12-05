a01c0ee134a773a469da6d762b563dcd74d99fcf
sofiehyllen <sofiehyllen@hotmail.com>
2024-12-05 19:12:30 +0100

    user_profile_deletion

--- static/ui.js
+++ static/ui.js
@@ -13,3 +13,26 @@ dropdowns.forEach(dropdown => {
 function showResults() {
     document.getElementById('search-results').classList.remove('hidden');
 }
+
+///////////////////////////////////////////////
+// Open and close modal for deleting profile
+document.addEventListener("DOMContentLoaded", function () {
+    const openModalBtn = document.getElementById("open-delete-modal-btn");
+    const deleteModal = document.getElementById("delete-modal");
+    const cancelDeleteBtn = document.getElementById("cancel-delete-btn");
+
+    // Open the modal when the "Delete account" button is clicked
+    openModalBtn.addEventListener("click", function (e) {
+        e.preventDefault(); 
+        deleteModal.classList.remove("hidden");
+        deleteModal.classList.add("flex");
+    });
+
+    // Close the modal when the "Cancel" button is clicked
+    cancelDeleteBtn.addEventListener("click", function (e) {
+        e.preventDefault();  
+        deleteModal.classList.remove("flex");
+        deleteModal.classList.add("hidden");
+    });
+});
+
